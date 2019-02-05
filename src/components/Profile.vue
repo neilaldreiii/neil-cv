929f16d4df70364d8ca182fad0337a025e776f5c
neil aldrei <neilaldrei@icloud.com>
2019-02-05 20:21:40 +0800

    Finished Profile

---
+++ src/components/Profile.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="profile">
+        <div class="profile-picture-container">
+            <div class="profile-picture"></div>
+        </div>
+        <div class="profile-info">
+            <h3>Peralta</h3>
+            <h1>Neil Aldrei</h1>
+            <p style="font-size: 10pt;">Web Developer</p>
+        </div>
+        <div class="profile-contacts">
+            <p>Naga City Camarines Sur PH</p>
+            <p>neilaldreiii@gmail.com</p>
+            <p>+63915-204-4704</p>
+        </div>
+        <div class="profile-social">
+            <a href="#" class="messenger">Messenger</a>
+            <a href="#" class="github">GitHub</a>
+            <a href="#" class="twitter">Twitter</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "profile",
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <profile></profile>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Profile from '@/components/Profile';
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    "profile": Profile
   }
 };
 </script>
