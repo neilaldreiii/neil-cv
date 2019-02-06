ee728c59548f123320bd17aab70a2b48ee9f20b0
neil aldrei <neilaldrei@icloud.com>
2019-02-06 17:35:52 +0800

    Finished Introduction

---
+++ src/components/Skills.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="sub-container">
+        <header class="header">
+            <h1>Skills</h1>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="container">
-    <profile></profile>
+    <app-profile></app-profile>
+    <div class="wrapper">
+      <app-intro></app-intro>
+      <app-skills></app-skills>
+    </div>
   </div>
 </template>
 
 <script>
 import Profile from '@/components/Profile';
+import Intro from '@/components/Introduction';
+import Skills from '@/components/Skills';
 
 export default {
   name: "home",
   components: {
-    "profile": Profile
+    "app-profile": Profile,
+    "app-intro": Intro,
+    "app-skills": Skills
   }
 };
 </script>
